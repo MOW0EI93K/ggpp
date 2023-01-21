54ad03b8475839f53c0282ffd5f8b4f5627b4445
MOW0EI93K <120707809+MOW0EI93K@users.noreply.github.com>
2023-01-21 15:04:45 -0800

    Update finder.py

--- finder.py
+++ finder.py
@@ -1,8 +1,9 @@
 from core.controllers import Controller
 from core.arguments import parse_args
 import multiprocessing
-
+from core.utils import main
 if __name__ == "__main__":
+    main()
     multiprocessing.freeze_support()
     controller = Controller(
         arguments=parse_args())
